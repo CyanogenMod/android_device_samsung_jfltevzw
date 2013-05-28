c0387f253a06e302438c370780dfa016a6fbafdc
Dan Pasanen <dan.pasanen@gmail.com>
2013-05-28 15:28:47 -0500

    jfltevzw: use common defconfigs from jf-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,8 +31,6 @@ TARGET_OTA_ASSERT_DEVICE := jfltevzw
 
 # Kernel
 TARGET_KERNEL_CONFIG         := jf_vzw_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := cyanogen_jf_defconfig
-TARGET_KERNEL_SELINUX_CONFIG := jfselinux_defconfig
 
 # loki
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/jfltevzw
