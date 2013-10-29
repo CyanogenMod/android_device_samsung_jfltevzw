aed95100ec25258ae1060832211fa548a2edee55
Dan Pasanen <dan.pasanen@gmail.com>
2013-10-29 17:56:39 -0500

    jfltevzw: update fingerprint VRUEMJ7
    Change-Id: I77c704acfe091aa36a714e774485f0616bbd8158

--- cm.mk
+++ cm.mk
@@ -6,7 +6,11 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jfltevzw \
+  TARGET_DEVICE=jfltevzw \
+  BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.3/JSS15J/I545VRUEMJ7:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltevzw-user 4.3 JSS15J I545VRUEMJ7 release-keys"
 
 PRODUCT_NAME := cm_jfltevzw
 PRODUCT_DEVICE := jfltevzw
