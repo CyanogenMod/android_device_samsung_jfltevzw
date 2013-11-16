502a46ce5400c9ce395c8b9275c848f53cff45be
Chris123NT <chris@chris123nt.com>
2013-11-16 18:22:42 -0500

    jfltevzw: access vzw apps in Play store
    Change-Id: Ib6d1861b0e32f0e39cacc46c9c186a02dccd6313

--- cm.mk
+++ cm.mk
@@ -12,6 +12,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.3/JSS15J/I545VRUEMJ7:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltevzw-user 4.3 JSS15J I545VRUEMJ7 release-keys"
 
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
 PRODUCT_NAME := cm_jfltevzw
 PRODUCT_DEVICE := jfltevzw
 
