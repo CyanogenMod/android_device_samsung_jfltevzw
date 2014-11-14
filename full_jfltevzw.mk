103eb3ed006105e1a7991519ed7237c4da6cdceb
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-14 15:51:53 -0600

    jfltevzw: update props, deps, blobs, de-unify
    Change-Id: I0d97ca7ebd0e8fdb55222c3e1d23ab8201f14524

--- full_jfltevzw.mk
+++ full_jfltevzw.mk
@@ -21,9 +21,9 @@
 # lines, full and maguro, hence its name.
 #
 
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from jfltevzw device
 $(call inherit-product, device/samsung/jfltevzw/device.mk)
 
