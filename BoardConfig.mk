d8b8035bc74d0232b9c9579319242b9fd2545190
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-14 15:51:53 -0600

    jfltevzw: update props, deps, blobs
    Change-Id: I0d97ca7ebd0e8fdb55222c3e1d23ab8201f14524

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@
 #
 
 # inherit from common JF
--include device/samsung/jf-common/BoardConfigCommon.mk
+include device/samsung/jf-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/jfltevzw/BoardConfigVendor.mk
