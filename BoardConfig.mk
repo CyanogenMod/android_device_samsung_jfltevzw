2c5fc507777321d2bff40285347cfa1ac67c99c4
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-14 15:51:53 -0600

    jfltevzw: update props, deps, blobs, de-unify
    Change-Id: I0d97ca7ebd0e8fdb55222c3e1d23ab8201f14524

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,16 +21,13 @@
 #
 
 # inherit from common JF
--include device/samsung/jf-common/BoardConfigCommon.mk
+include device/samsung/jf-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/jfltevzw/BoardConfigVendor.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := jfltevzw
-
-# Kernel
-TARGET_KERNEL_CONFIG         := jf_vzw_defconfig
+TARGET_OTA_ASSERT_DEVICE := jfltevzw,jflte
 
 # loki
-TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/jf-common/loki
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/jf-common/releasetools
