3e9c05e4c4ecb55d9b7437ec9b4a8bd02852d8b5
Grando F <aqdevmain@gmail.com>
2016-08-03 11:48:27 -0700

    jfltevzw: Remove libstlport
    Move up to common
    
    Change-Id: I385c7173a9dcd407cc747925161d78b64ab1c66f

--- full_jfltevzw.mk
+++ full_jfltevzw.mk
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- 
+
 #
 # This file is the build configuration for a full Android
 # build for maguro hardware. This cleanly combines a set of
@@ -37,7 +37,3 @@ PRODUCT_MODEL := SCH-I545
 # Qualcomm
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
-
-# Stlport
-PRODUCT_PACKAGES += \
-    libstlport
